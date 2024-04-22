0ddbe6d125745138fd5cfce1fdd5cfd7620db4e6
Acrohs <67403739+Acrohs@users.noreply.github.com>
2024-04-22 18:03:06 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+<html lang="en"><head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/styles.css"/>
+    <style>.logo{position:absolute;bottom:16px;right:16px;width:32px;height:32px;z-index:2;background-image:url(https://app.spline.design/_assets/_icon_favicon_16x16.png);background-size:cover;}.logo:hover^{opacity0.8;}    
+
+    </style>
+</head>
+<body>
+    <<canvas id="canvas3d"></canvas>
+    <<a class="logo" href="http://spline,design"></a>
+    <script async src="http://unpkg.com/es-moduleshims@1.3.6/es-module-shims.js"></script>
+    <<script type="importmap">
+        {
+            "imports": {
+                "@splinetool/runtime": "https://unpkg.com/@splinetool/runtime@1.0.66/build/runetime.js"
+            }
+        }
+        
+    </script>        
+    <script>type="module" src="./src/main.js"</script>
+</body>
+</html>
